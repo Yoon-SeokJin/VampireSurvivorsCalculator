8bc1a0db0942adc9a58d09a5efa06c2b1ee9b121
YoonSeokJin <seokjin1013@gmail.com>
2022-04-24 16:33:27 +0900

    Update new power up 0.5.103 version

--- lib/item_slider_list.dart
+++ lib/item_slider_list.dart
@@ -58,7 +58,7 @@ class ItemSliderList extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(AppLocalizations.of(context)!
-                  .versionInfo('0.5.0f', AppLocalizations.of(context)!.dateFormat(14, 4, 2022))),
+                  .versionInfo('0.5.103', AppLocalizations.of(context)!.dateFormat(24, 4, 2022))),
             ),
             const Spacer(),
             ElevatedButton(
